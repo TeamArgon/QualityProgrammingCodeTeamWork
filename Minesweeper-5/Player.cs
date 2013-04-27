33c8a004f24f18610c8fa2dd1d8092a5e05effb2
Petar Yankov <peshopit@yahoo.com>
2013-04-27 15:26:30 +0300

    Add XML comments

--- Minesweeper-5/Player.cs
+++ Minesweeper-5/Player.cs
@@ -1,28 +1,63 @@
-﻿namespace Minesweeper
+﻿//-----------------------------------------------------------------------
+// <copyright file="Player.cs" company="TelerikAcademy">
+// All rights reserved © Telerik Academy 2012-2013
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Minesweeper
 {
     using System;
 
+    /// <summary>
+    /// Represents a game player
+    /// </summary>
     public class Player : IComparable
     {
+        /// <summary>
+        /// Player's name
+        /// </summary>
         private string name;
+
+        /// <summary>
+        /// Player's score
+        /// </summary>
         private int score;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Player" /> class.
+        /// </summary>
+        /// <param name="name">Player's name</param>
+        /// <param name="score">Player's score</param>
         public Player(string name, int score)
         {
             this.name = name;
             this.score = score;
         }
 
+        /// <summary>
+        /// Gets the name of the player
+        /// </summary>
         public string Name
         {
             get { return this.name; }
         }
 
+        /// <summary>
+        /// Gets the score of the player
+        /// </summary>
         public int Score
         {
             get { return this.score; }
         }
 
+        /// <summary>
+        /// Compares two instances of the class <see cref="Player" /> by their scores
+        /// </summary>
+        /// <param name="obj">The other player</param>
+        /// <returns>
+        /// -1 if this player's scores are bigger than the other player's score
+        /// 1 if this player's scores are smaller than the other player's score
+        /// </returns>
         public int CompareTo(object obj)
         {
             if (!(obj is Player))
@@ -33,6 +68,13 @@
             return -1 * this.score.CompareTo(((Player)obj).score);
         }
 
+        /// <summary>
+        /// Converts an instance of the class <see cref="Player" /> into a string
+        /// </summary>
+        /// <returns>
+        /// A string containing player's name and player's score. Example:
+        /// Ivan --> 100
+        /// </returns>
         public override string ToString()
         {
             string result = this.name + " --> " + this.score;
