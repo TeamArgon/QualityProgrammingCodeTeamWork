fdfd3a8057f1efb0625305d1e2b2886952883fc8
Petar Yankov <peshopit@yahoo.com>
2013-05-11 00:09:36 +0300

    Add MinesweeperUnitTest project

---
+++ MinesweeperUnitTests/BoardTest.cs
@@ -0,0 +1,31 @@
+﻿namespace MinesweeperUnitTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Minesweeper;
+
+    [TestClass]
+    public class BoardTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestBoardConstructor1_ThrowsExcepsion()
+        {
+            Board board = new Board(-1, 10, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestBoardConstructor2_ThrowsExcepsion()
+        {
+            Board board = new Board(10, 0, 3);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestBoardConstructor3_ThrowsExcepsion()
+        {
+            Board board = new Board(10, 5, 51);
+        }
+    }
+}
