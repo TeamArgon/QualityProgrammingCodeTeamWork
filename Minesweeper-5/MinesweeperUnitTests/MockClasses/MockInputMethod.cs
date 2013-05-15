5f6d2a19cce711a7e321d9a5b768aa658526c251
Vasil Dininski <dininski@gmail.com>
2013-05-15 21:03:37 +0300

    Added command executor and mock tests

---
+++ Minesweeper-5/MinesweeperUnitTests/MockClasses/MockInputMethod.cs
@@ -0,0 +1,19 @@
+﻿using Minesweeper.InputMethods;
+
+namespace MinesweeperUnitTests.MockClasses
+{
+    public class MockInputMethod : IInputMethod
+    {
+        string mockInput;
+
+        public void SetInput(string mockInput)
+        {
+            this.mockInput = mockInput;
+        }
+
+        public string GetUserInput()
+        {
+            return mockInput;
+        }
+    }
+}
---
+++ Minesweeper-5/MinesweeperUnitTests/MockClasses/MockRenderer.cs
@@ -0,0 +1,49 @@
+﻿namespace MinesweeperUnitTests.MockClasses
+{
+    using Minesweeper.Renderers;
+    using Minesweeper.Common;
+    using System.Text;
+
+    public class MockRenderer : IRenderer
+    {
+        readonly StringBuilder mockMessage;
+        readonly StringBuilder mockError;
+        Board mockBoard;
+
+        public MockRenderer()
+        {
+            this.mockMessage = new StringBuilder();
+            this.mockError = new StringBuilder();
+        }
+
+        public string GetMessage()
+        {
+            return this.mockMessage.ToString();
+        }
+
+        public string GetError()
+        {
+            return this.mockError.ToString();
+        }
+
+        public Board GetBoard()
+        {
+            return this.mockBoard;
+        }
+
+        public void DrawBoard(Board board)
+        {
+            this.mockBoard = board;
+        }
+
+        public void DisplayMessage(string message)
+        {
+            this.mockMessage.Append(message);
+        }
+
+        public void DisplayError(string error)
+        {
+            this.mockError.Append(error);
+        }
+    }
+}
